b4bd9e8532c8538b34b982a618145ea6791d43f7
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-10-31 10:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function updateTimer() {
+      // let now = new Date();
+      // let  dateTime = now.toString();
+      document.getElementById("timer").innerHTML =new Date();
+    }
+    updateTimer();
+    setInterval(updateTimer, 1000);
